7588e4a6483ea6b6d13077d85b41128574a77064
Vamshee Krishna Paleti <>
2020-10-16 21:51:09 +0530

    implemented Registration fee details.

--- app/src/main/java/com/rmart/authentication/views/RegistrationFragment.java
+++ app/src/main/java/com/rmart/authentication/views/RegistrationFragment.java
@@ -5,21 +5,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.rmart.BuildConfig;
 import com.rmart.R;
 import com.rmart.utilits.RetrofitClientInstance;
 import com.rmart.utilits.Utils;
+import com.rmart.utilits.pojos.RegistrationFeeStructure;
 import com.rmart.utilits.pojos.RegistrationResponse;
 import com.rmart.utilits.services.AuthenticationService;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatEditText;
+import androidx.appcompat.widget.AppCompatTextView;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -28,6 +33,7 @@ public class RegistrationFragment extends LoginBaseFragment implements View.OnCl
 
     AppCompatEditText tvFirstName, tvLastName, tVMobileNumber, tvEmail, tvPassword, tvConformPassword;
 
+    ArrayList<RegistrationFeeStructure> registrationFeeStructures = new ArrayList<>();
     public RegistrationFragment() {
         // Required empty public constructor
     }
@@ -39,8 +45,27 @@ public class RegistrationFragment extends LoginBaseFragment implements View.OnCl
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        getFeeDetails();
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_registration, container, false);
+    }
+
+    private void getFeeDetails() {
+        RegistrationFeeStructure registration = new RegistrationFeeStructure();
+        registration.setPayType("Registration fee");
+        registration.setAmount("2000");
+
+        RegistrationFeeStructure gst = new RegistrationFeeStructure();
+        gst.setPayType("GST 13%");
+        gst.setAmount("260");
+
+        RegistrationFeeStructure total = new RegistrationFeeStructure();
+        total.setPayType("Total amount");
+        total.setAmount("2260");
+
+        registrationFeeStructures.add(registration);
+        registrationFeeStructures.add(gst);
+        registrationFeeStructures.add(total);
     }
 
     @Override
@@ -54,11 +79,19 @@ public class RegistrationFragment extends LoginBaseFragment implements View.OnCl
         tvEmail = view.findViewById(R.id.email);
         tvPassword = view.findViewById(R.id.password);
         tvConformPassword = view.findViewById(R.id.confirm_password);
+        LinearLayout  paymentBase = view.findViewById(R.id.payment_base);
         if(BuildConfig.ROLE_ID.equalsIgnoreCase(Utils.RETAILER_ID)) {
-            view.findViewById(R.id.payment_text).setVisibility(View.VISIBLE);
+            paymentBase.setVisibility(View.VISIBLE);
+            for (RegistrationFeeStructure feeStructure :
+                    registrationFeeStructures) {
+                LinearLayout row = (LinearLayout) requireActivity().getLayoutInflater().inflate(R.layout.layout, null);
+                ((AppCompatTextView)row.findViewById(R.id.pay_type)).setText(feeStructure.getPayType());
+                ((AppCompatTextView)row.findViewById(R.id.pay_amt)).setText(feeStructure.getAmount());
+                paymentBase.addView(row);
+            }
             register.setText(R.string.proceed_to_pay);
         } else {
-            view.findViewById(R.id.payment_text).setVisibility(View.GONE);
+            paymentBase.setVisibility(View.GONE);
             register.setText(R.string.register_right);
         }
     }
---
+++ app/src/main/java/com/rmart/utilits/pojos/RegistrationFeeStructure.java
@@ -0,0 +1,30 @@
+package com.rmart.utilits.pojos;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import com.rmart.baseclass.views.BaseFragment;
+
+public class RegistrationFeeStructure extends BaseResponse {
+    @SerializedName("pay_type")
+    @Expose
+    String payType;
+    @SerializedName("amount")
+    @Expose
+    String amount;
+
+    public String getPayType() {
+        return payType;
+    }
+
+    public void setPayType(String payType) {
+        this.payType = payType;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+}
