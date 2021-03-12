cb77ebdc9b405e4d9e468a1ae4fef8a62f65a372
Shweta <shwetadalvi9@gmail.com>
2021-03-12 19:40:17 +0530

    new home page repository
    home page

---
+++ app/src/main/java/com/rmart/customer/shops_new/home/repositories/ShopRepository.java
@@ -0,0 +1,58 @@
+package com.rmart.customer.shops_new.home.repositories;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.rmart.customer.shops_new.home.api.Shops;
+import com.rmart.customer.shops_new.home.model.ShopHomePageResponce;
+import com.rmart.profile.model.MyProfile;
+import com.rmart.utilits.RetrofitClientInstance;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.rmart.utilits.Utils.CLIENT_ID;
+
+public class ShopRepository {
+
+    public static MutableLiveData<ShopHomePageResponce> getShopHomePageNEW(){
+
+        Shops shope = RetrofitClientInstance.getRetrofitInstance().create(Shops.class);
+        final MutableLiveData<ShopHomePageResponce> resultMutableLiveData = new MutableLiveData<>();
+        String type = MyProfile.getInstance().getRoleID();
+        Call<ShopHomePageResponce> call = shope.getShopHomePageNEW(CLIENT_ID);
+        final ShopHomePageResponce result = new ShopHomePageResponce();
+
+        call.enqueue(new Callback<ShopHomePageResponce>() {
+            @Override
+            public void onResponse(Call<ShopHomePageResponce> call, Response<ShopHomePageResponce> response) {
+                ShopHomePageResponce data = response.body();
+                if(data!=null) {
+                    resultMutableLiveData.setValue(data);
+
+                } else {
+                    result.setMsg(data.getMsg());
+                    result.setStatus("400");
+                    resultMutableLiveData.setValue(result);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ShopHomePageResponce> call, Throwable t) {
+                if(t.getLocalizedMessage().equalsIgnoreCase("Unable to resolve host \"hungryindia.co.in\": No address associated with hostname"))
+                { result.setMsg("Please Check Enternet Connection");
+
+                } else {
+                    result.setMsg(t.getLocalizedMessage());
+                }
+                resultMutableLiveData.setValue(result);
+
+
+            }
+        });
+        return resultMutableLiveData;
+
+    }
+
+
+}
