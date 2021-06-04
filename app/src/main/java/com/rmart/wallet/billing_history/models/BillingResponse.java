93baa48b36cec1bd32cdac9a1a0fd4308c4ec3d1
Akshata <akshata.bookonspot@gmail.com>
2021-06-04 19:45:44 +0530

    Billing History
    Billing History

---
+++ app/src/main/java/com/rmart/wallet/billing_history/models/BillingResponse.java
@@ -0,0 +1,57 @@
+
+package com.rmart.wallet.billing_history.models;
+
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+public class BillingResponse {
+
+    @SerializedName("status")
+    @Expose
+    private Integer status;
+    @SerializedName("msg")
+    @Expose
+    private String msg;
+    @SerializedName("data")
+    @Expose
+    private List<Datum> data = null;
+    @SerializedName("request_id")
+    @Expose
+    private Integer requestId;
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public List<Datum> getData() {
+        return data;
+    }
+
+    public void setData(List<Datum> data) {
+        this.data = data;
+    }
+
+    public Integer getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(Integer requestId) {
+        this.requestId = requestId;
+    }
+
+}
